572dc96ed807a523e6494d85860187df96d3d39a
taka-toshi <taka.apg5jgfrm720ars@gmail.com>
2022-07-31 13:22:30 +0900

    3rd commit

--- sample.js
+++ sample.js
@@ -5,6 +5,22 @@ const logElem = document.getElementById("log");
 const startElem = document.getElementById("start");
 const stopElem = document.getElementById("stop");
 
+// timer
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+// timer
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
 // Options for getDisplayMedia()
 
 var displayMediaOptions = {
@@ -22,6 +38,33 @@ startElem.addEventListener("click", function (evt) {
 stopElem.addEventListener("click", function (evt) {
   stopCapture();
 }, false);
+
+// timer
+startBtn.addEventListener("click", () => {
+  if(paused){
+    paused = false;
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime,75); 
+  }
+});
+pauseBtn.addEventListener("click", () => {
+  if(!paused){
+    paused = true;
+    elapsedTime = Date.now() - startTime
+    clearInterval(intervalId);
+  }
+});
+resetBtn.addEventListener("click", () => {
+  paused =true;
+  clearInterval(intervalId);
+  startTime = 0;
+  elapsedTime = 0;
+  currentTime = 0;
+  hrs = 0;
+  mins = 0;
+  secs = 0;
+  timeDisplay.textContent = "00:00:00"
+});
 
 console.log = msg => logElem.innerHTML += `${msg}<br>`;
 console.error = msg => logElem.innerHTML += `<span class="error">${msg}</span><br>`;
@@ -55,3 +98,21 @@ function dumpOptionsInfo() {
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
 
+// timer
+function updateTime(){
+  elapsedTime = Date.now() - startTime;
+
+  secs = Math.floor((elapsedTime/1000)%60);
+  mins = Math.floor((elapsedTime/(1000*60))%60);
+  hrs = Math.floor((elapsedTime/(1000*60*60))%60);
+
+  secs = pad(secs);
+  mins = pad(mins);
+  hrs = pad(hrs);
+  timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+
+  function pad(unit){
+    return (("0") + unit).length > 2 ? unit : "0" + unit;
+  }
+}
